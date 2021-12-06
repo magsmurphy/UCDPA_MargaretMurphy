ae166a58757c4f803e97652d6e7e089841d8e176
Margaret Murphy <mmurphy@crokepark.ie>
2021-12-06 10:49:51 +0000

    'Update'

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+# Import packages
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import pandas as pd
+
+# Import Data
+schdeb = pd.read_excel('Debtors.xlsx', sheet_name='Scheduled')
+upfdeb = pd.read_excel('Debtors.xlsx', sheet_name='Upfront')
+curdeb = pd.read_excel('Debtors.xlsx', sheet_name='Current')
+
+# Clean Data to enable merge
+schdeb.rename(columns={'CUSTNMBR': 'acct_id','PAYMENT AMOUNT':'amount', 'DUEDATE': 'payment date' }, inplace =True)
+schdeb3col = schdeb[['acct_id', 'amount', 'payment date']]
+mask = (schdeb3col['payment date'] >= '01-10-2021')
+schdeb3col = schdeb3col.loc[mask]
+
+upfdeb['payment date'] = '2022-01-01'
+upfdeb.rename(columns={'report_owed_amount':'amount'}, inplace='True')
+upfdeb3col = upfdeb[['acct_id', 'amount', 'payment date']]
+
+curdeb['payment date'] = '2022-01-01'
+curdeb.rename(columns={'Customer Number':'acct_id', 'Customer Balance': 'amount'}, inplace=True)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
